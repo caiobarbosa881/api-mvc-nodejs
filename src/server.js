39f2039f8589d3c8b7aec0d057893d0ce1797e36
Caio <caiobarbosasoriano@gmail.com>
2023-02-23 20:40:37 -0300

    refactor: removendo importação não utilizada no arquivo server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const User = require("./models/User");
 const db = require("./database/db");
 
 const routes = require("./routes/routes")
