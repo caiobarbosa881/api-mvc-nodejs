986509ea227e7f03d21344d15e57960bd70453e4
Caio <caiobarbosasoriano@gmail.com>
2023-02-25 14:24:02 -0300

    test: Novos Testes para rota /

--- routes.test.js
+++ routes.test.js
@@ -2,14 +2,21 @@ const supertest = require('supertest');
 const app = require('./src/server');
 const request = supertest(app);
 
-describe('Testando Rota /', () => {
-  describe('Acessando rota utilizando o método get', () => {
-    it('A rota deve ser possível de ser acessada e retorna um status 200', async () => {
-
-      const response = await request.get('/');
-
+describe('GET /', () => {
+  describe('Testando Rotas', () => {
+    it('Deve Retornar o Status HTTP 200', async () => {
+      const response = await request.get('/'); 
       expect(response.status).toBe(200);
+    });
 
-    })
+    it('Retorna um array userData', async () => {
+      const response = await request.get('/'); 
+      expect(Array.isArray(response.body.userData)).toBe(true);
+    });
+
+    it('Deve retornar uma resposta JSON', async () => {
+      const response = await request.get('/'); 
+      expect(response.type).toBe('application/json');
+    });
   });
 });
