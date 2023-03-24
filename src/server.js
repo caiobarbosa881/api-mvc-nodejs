256927311a9044f0c5605e28452d903b13947ecb
Caio <caiobarbosasoriano@gmail.com>
2023-03-24 18:30:24 -0300

    feat: removendo linhas desnecessárias não utilizadas no server.js

--- src/server.js
+++ src/server.js
@@ -2,17 +2,11 @@
 
 const express = require("express");
 const app = express();
-const path = require("path");
 const db = require("./database/db");
 
 const routes = require("./routes/routes")
 
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname + "/public")));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname + "/views"));
 
 app.use("/", routes);
 
